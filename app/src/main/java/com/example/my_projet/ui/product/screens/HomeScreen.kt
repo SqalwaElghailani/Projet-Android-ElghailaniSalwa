6169dd4c2ae03d819fe026eb9fd1cdc7bfd3e0a7
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-06-24 22:19:29 +0200

    Mise à jour de HomeScreen

--- app/src/main/java/com/example/my_projet/ui/product/screens/HomeScreen.kt
+++ app/src/main/java/com/example/my_projet/ui/product/screens/HomeScreen.kt
@@ -1,7 +1,13 @@
 package com.example.my_projet.ui.product.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.my_projet.data.Entities.Product
 import com.example.my_projet.ui.product.component.MainHeader
 import com.example.my_projet.ui.product.component.ProductGrid
@@ -25,16 +31,27 @@ fun HomeScreen(
     onNavigateToDetails: (String) -> Unit,
     onPreviousTop: () -> Unit,
     onNextTop: () -> Unit,
-    onAddToCart: (Product) -> Unit
+    onAddToCart: (Product) -> Unit,
+    navController: NavController,        // ✅ أضف هذا
+    userId: Int = 1
 ) {
-    Column {
+    Column (
+            modifier = Modifier
+                .padding(start = 4.dp, end = 4.dp, top = 20.dp, bottom = 20.dp)  // تقدر تحكم في البادينغ من هنا
+        ) {
         MainHeader(
             searchTerm = searchTerm,
             onSearchChange = onSearchChange,
             onSearchSubmit = onSearchSubmit,
             cartCount = cartCount,
             onNavigateToOrders = onNavigateToOrders,
-            onNavigateToCart = onNavigateToCart
+            onNavigateToCart = {
+                Log.d("NAVIGATION", "Navigating to cart screen from HomeScreen")
+                onNavigateToCart()
+            },
+            navController = navController,
+            userId = userId
+
 
         )
         if (topProducts.isNotEmpty()) {
